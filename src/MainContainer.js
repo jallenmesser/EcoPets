790fd937b5ed26a0eecf12d9140843d3ef53c863
jallenmesser <jallenmessersmith@gmail.com>
2021-02-14 21:03:02 -0500

    second commit

---
+++ src/MainContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function MainContainer(props) {
+    return (
+        <div>
+            <p>I am the main container</p>
+        </div>
+    )
+}
+export default MainContainer
---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Nav(props) {
+    return (
+        <div>
+            <p>I am the nav</p>
+        </div>
+    )
+}
+export default Nav
